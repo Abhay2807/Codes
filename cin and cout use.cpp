51130840a276f98662118abf2cf30b392c531782
Abhay2807 <76277587+Abhay2807@users.noreply.github.com>
2021-07-16 09:06:07 +0530

    Add files via upload

---
+++ cin and cout use.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int a=28;
+ cout<<"My fav. no. is "<< a<<endl;
+ int b;
+ cout<<"Enter your fav. no. please:";
+ cin>>b;
+ cout<<"Your fav. no. is "<< b;
+ return 0;
+}
