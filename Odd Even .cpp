c958eeef84c5d3429b03b7d6db2ea144dcfd774b
ABHAY SINGH <76277587+Abhay2807@users.noreply.github.com>
2021-07-19 21:19:09 +0530

    Add files via upload

---
+++ Odd Even .cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+int num;
+cout<<"Please enter the number: "<<endl;
+cin>>num;
+if(num%2==0){
+cout<<num<<" is an even number";
+}
+else{
+cout<<num<<" is an odd number";
+}
+return 0;
+}
