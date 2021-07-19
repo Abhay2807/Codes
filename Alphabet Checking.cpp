64170cfd7f6c9b039ddd5fdeeda3ed8e70a166d8
ABHAY SINGH <76277587+Abhay2807@users.noreply.github.com>
2021-07-19 21:21:11 +0530

    Add files via upload

---
+++ Alphabet Checking.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{ char g;
+cin>>g;
+if(g=='a'||g=='e'||g=='i'||g=='o'||g=='u'||g=='A'||g=='E'||g=='I'||g=='O'||g=='U')
+{ cout<<g<<" is an vowel alphabet";
+}
+else{
+cout<<g<<" is a consonant alphabet";
+}
+return 0;
+}
